2eea74fb02748098951b07025fc689594455da03
Keith Meyer <th3.k317h@gmail.com>
2016-03-29 17:25:49 -0400

    TempFile

--- ModemChecker.py
+++ ModemChecker.py
@@ -16,12 +16,25 @@ if password:
     tn.write(password.encode('ascii') + b"\n")
 #configuration on modem is dump in telnet client
 tn.write(b"dumpcfg 1\n")
-
-
-tn.write(b"exit\n")
-#telnet session is stored in this variable
+tn.write(b"exit/n")
 configdump = (tn.read_all().decode('ascii'))
-if '<DHCPServerEnable>' in configdump:
-	print("Present")
+
+
+#Temp file bits
+fp = tempfile.TemporaryFile()
+fp.write(configdump.encode())
+fp.seek(0)
+print(fp.read())
+fp.close()
+
+
+
+
+
+
+
+
+
+
 
 
